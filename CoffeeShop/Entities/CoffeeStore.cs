cb22154acfbb435d1b72308efc7a20d8a3cd280b
xvanc-yurii <yurii.ivanchenko.plt@gmail.com>
2023-03-05 10:12:27 +0200

    Создана сущность CoffeeStore, в которой описан сам 'чек' (наименование, дата продажи, цена, количество). Создан интерфейс IContext и наследуемый от него класс CoffeeContext, который описывает весь функционал

---
+++ CoffeeShop/Entities/CoffeeStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Entities
+{
+    public class CoffeeStore
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
